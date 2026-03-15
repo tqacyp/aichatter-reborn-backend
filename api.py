eeeb78daa0b87b967a2dc5412f2fae574eb26665
tqacyp <tqacyp@163.com>
2026-03-15 16:53:18 +0800

    解析请求

--- api.py
+++ api.py
@@ -27,6 +27,16 @@ class DeepSeekAPI:
         Yields:
             dict: 流式响应数据块
         """
+        
+        """
+            返回格式示例：
+            {
+                "success": True
+                "done": False
+                "reasoning": True
+                "message_delta": "Hmm..."
+            }
+        """
         if messages is None:
             raise ValueError("Messages can't be empty")
         
@@ -55,10 +65,62 @@ class DeepSeekAPI:
                     line=line.decode("utf-8")
                     if line.startswith("data:"):
                         data=line[5:].strip()
+                        if data=="[DONE]":
+                            # 解析完毕
+                            yield {
+                                "success": True,
+                                "done": True,
+                                "message": ""
+                            }
+                            continue
                         if data:
-                            text_stream=json.loads(data)
-                            print(text_stream)
+                            try:
+                                chunk=json.loads(data)
+                            except Exception:
+                                # 处理json解析失败
+                                print(f"跳过无法解析的块:{data}")
+                                continue
+                            if isinstance(chunk, dict) and 'choices' in chunk and len(chunk['choices']) > 0:
+                                # 是否思考均需要的解析
+                                delta = chunk['choices'][0].get('delta', {}) or {}
+                                if thinking and delta["reasoning_content"] is not None and delta["content"] is None:
+                                    # 思考开启，且返回思考内容
+                                    delta_reasoning = delta["reasoning_content"]
+                                    yield {
+                                        "success": True,
+                                        "done": False,
+                                        "reasoning": True,
+                                        "message_delta": delta_reasoning
+                                    }
+                                elif thinking and delta["reasoning_content"] is None and delta["content"] is not None:
+                                    # 思考开启，且返回结果内容
+                                    delta_message = delta["content"]
+                                    yield {
+                                        "success": True,
+                                        "done": False,
+                                        "reasoning": False,
+                                        "message_delta": delta_message
+                                    }
+                                elif not thinking and delta["content"] is not None:
+                                    # 思考关闭，返回结果内容
+                                    delta_message = delta["content"]
+                                    yield {
+                                        "success": True,
+                                        "done": False,
+                                        "reasoning": False,
+                                        "message_delta": delta_message
+                                    }
+                                else:
+                                    # 这他妈是什么东西
+                                    print(f"解析失败:delta:{delta}")
+                                    continue
+                            else:
+                                print(f"跳过非返回文字内容:{chunk}")
+                                continue
+                            
+                                                        
         except requests.exceptions.RequestException as e:
+            # 处理HTTP请求错误
             yield {
                 "success": False,
                 "message": f"API请求错误:{str(e)}",
